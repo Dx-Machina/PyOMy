dbafb86920e8d519f1b60017cf0894d9981d8dcd
Youssef Hegazy <You7Joe@iCloud.com>
2023-12-05 10:25:20 -0800

    added voices.py and display text in console

---
+++ voices.py
@@ -0,0 +1,12 @@
+#import speech_recognition as sr
+import pyttsx3
+import openai
+
+voices = engine.getProperty('voices')
+for voice in voices:
+    print("Voice:")
+    print(" - ID: %s" % voice.id)
+    print(" - Name: %s" % voice.name)
+    print(" - Languages: %s" % voice.languages)
+    print(" - Gender: %s" % voice.gender)
+    print(" - Age: %s" % voice.age)

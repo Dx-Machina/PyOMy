87ba11bf881d9335db0e61664ae8f05ff4a4993d
Youssef Hegazy <You7Joe@iCloud.com>
2023-12-06 14:38:06 -0800

    added a shell script to initialize the environment and cleaned up the main script a bit

---
+++ InitializerShell.bashrc
@@ -0,0 +1,8 @@
+export VOICE_RATE=150
+export VOICE_VOLUME=0.8
+export VOICE='english'
+export OPENAI_API_KEY='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'
+
+#Run the python file
+
+python PyOMyCoPi.py
--- PyOMyCoPi.py
+++ PyOMyCoPi.py
@@ -3,7 +3,6 @@ import speech_recognition as sr
 import pyttsx3
 import openai
 
-listening = True
 # Initialize pyttsx3
 def initialize_pyttsx3():
     engine = pyttsx3.init()
@@ -13,7 +12,7 @@ def initialize_pyttsx3():
     return engine
 
 # Initialize speech recognition
-def initialize_speech_recognition():
+def initialize_speech_recognition(source):
     recognizer = sr.Recognizer()
     recognizer.dynamic_energy_threshold = 3000
     recognizer.adjust_for_ambient_noise(source)
@@ -36,31 +35,43 @@ def get_response(user_input):
     print("Jarvis: " + ChatGPT_reply)# Print the response to the console
     return ChatGPT_reply
 # Set voice properties
-def set_voice_properties(engine, rate=120, volume=1.0, voice='greek'):
+def set_voice_properties(engine):
+    rate = os.getenv('VOICE_RATE', 120)
+    volume = os.getenv('VOICE_VOLUME', 1.0)
+    voice = os.getenv('VOICE', 'greek')   
     engine.setProperty('rate', rate)
     engine.setProperty('volume', volume)
     engine.setProperty('voice', voice)
 
 # Main loop
-while listening:
-    with sr.Microphone() as source:
-        engine = initialize_pyttsx3()
-        recognizer = initialize_speech_recognition()
+def main_loop():
+    listening = True
+    while listening:
+        with sr.Microphone() as source:
+            engine = initialize_pyttsx3()
+            recognizer = initialize_speech_recognition(source)
 
-        try:
-            print("Listening...")
-            audio = recognizer.listen(source, timeout=5.0)
-            response = recognizer.recognize_google(audio)
-            print(response)
-           
-            if "jarvis" in response.lower():   
-                response_from_openai = get_response(response)
-                set_voice_properties(engine)
-                engine.say(response_from_openai)
-                engine.runAndWait()               
-            else:
-                print("Didn't recognize 'jarvis'.")
-        except sr.WaitTimeoutError:
-            print("Timeout; no speech heard")
-        except Exception as e:
-            print(f"An error of type {type(e).__name__} occurred: {e}")
+            try:
+                print("Listening...")
+                audio = recognizer.listen(source, timeout=5.0)
+                response = recognizer.recognize_google(audio)
+                print(response)
+            
+                if "jarvis" in response.lower():   
+                    response_from_openai = get_response(response)
+                    set_voice_properties(engine)
+                    engine.say(response_from_openai)
+                    engine.runAndWait()               
+                else:
+                    print("Didn't recognize 'jarvis'.")
+            except sr.WaitTimeoutError:
+                print("Timeout; no speech heard")
+            except KeyboardInterrupt:
+                print("Keyboard interrupt")
+                listening = False
+            except Exception as e:
+                print(f"An error of type {type(e).__name__} occurred: {e}")
+
+# Run the main loop
+if __name__ == "__main__":
+    main_loop()

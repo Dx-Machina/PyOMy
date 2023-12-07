de195fe379e6275366119fa56c6b5208f6b367f6
Youssef Hegazy <123545124+Dx-Machina@users.noreply.github.com>
2023-12-06 18:02:20 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ sudo apt-get install espeak # Install the dependencies
 sudo apt-get install flac   # Install the dependencies
 
 # Set the environment variables
-export VOICE_RATE=100 # words per minute
+export VOICE_RATE=80 # words per minute
 export VOICE_VOLUME=1.0 # float between 0 and 1
 export VOICE='english' # sets the language to english
 export OPENAI_API_KEY='Your Personal OpenAI API Key' # put your OpenAI API key between the quotes.
